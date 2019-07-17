bb5c024bdfb6896c604c6bf2b2148229b3f1734c
khetanvallurupalli <32341967+khetanvallurupalli@users.noreply.github.com>
2019-07-17 10:01:41 -0400

    Update env.js

--- src/config/env.js
+++ src/config/env.js
@@ -7,7 +7,7 @@ const logger = require('../config/log')
 
 const CONFIG = {
     DEV: {
-        DATABASE_URL: 'postgres://postgres:postgres@10.202.70.111:5432/postgres'
+        DATABASE_URL: 'postgres://ojxlxatzphvhvq:a289f897a712c071d68f89c489020ff94d258103884eabc1564a42fbe0f0fae4@ec2-174-129-227-80.compute-1.amazonaws.com:5432/dd9su3rp12rvm'
         , SERVER_PORT: 4001
     },
     PRODUCTION: {
