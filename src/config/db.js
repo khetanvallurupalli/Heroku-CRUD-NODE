32d65e65aeccd39e4e37af5db859dfee1eabc44a
khetanvallurupalli <32341967+khetanvallurupalli@users.noreply.github.com>
2019-07-17 10:04:36 -0400

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -5,8 +5,7 @@ console.log('env', env.DATABASE_URL);
 
 const connectionString = env.DATABASE_URL
 const client = new Client({
-    connectionString,
-    ssl: true,
+    connectionString : "postgres://ojxlxatzphvhvq:a289f897a712c071d68f89c489020ff94d258103884eabc1564a42fbe0f0fae4@ec2-174-129-227-80.compute-1.amazonaws.com:5432/dd9su3rp12rvm",
 });
 
 client.connect().then(r => {
